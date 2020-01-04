dc12983bf97919691904a38879e4dd36907ca24d
超奇 <252261109@qq.com>
2020-01-04 14:35:25 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+//  导入按需加载 vant 组件
+import '../plugins/vant'
+// 导入axios
+import axios from 'axios'
+Vue.prototype.$http = axios
 
 Vue.config.productionTip = false
 
